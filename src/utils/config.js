59557e22e373dbfdc9c335535e928cdc3a7ecbc8
afanimartin <afani.dev@gmail.com>
2024-06-18 13:49:44 +0200

    Update database config

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,9 +3,9 @@ dotenv.config();
 
 const config = {
   MONGODB_URI:
-    process.env.NODE_ENV === "test"
-      ? process.env.TEST_MONGODB_URI
-      : process.env.DEV_MONGODB_URI,
+    process.env.NODE_ENV === "development"
+      ? process.env.DEV_MONGODB_URI
+      : process.env.PROD_MONGODB_URI,
   PORT: process.env.PORT
 };
 
