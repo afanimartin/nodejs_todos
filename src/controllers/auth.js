97076d2d9e986f748ee853007d26a35cf3e9d84b
afanimartin <afani.dev@gmail.com>
2024-06-20 14:22:17 +0200

    Add endpoint for logging in

---
+++ src/controllers/auth.js
@@ -0,0 +1,14 @@
+import bcrypt from "bcrypt";
+import User from "../models/user.js";
+
+const isUserValid = async (authUser) => {
+  const user = await User.findOne({ username: authUser.username });
+  const isPasswordCorrect =
+    user === null
+      ? false
+      : await bcrypt.compare(authUser.password, user.passwordHash);
+
+  return { user: user, isExist: isPasswordCorrect };
+};
+
+export { isUserValid };
